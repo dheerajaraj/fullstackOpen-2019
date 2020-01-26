5a21a7b801b339c58700b30600f0f84bf9ce6007
dheerajtaiger <dheerajaraj@yahoo.com.sg>
2020-01-27 03:07:12 +0800

    completed week1-Ex1.4

--- reactOpenCourse/appeng/src/AppPart1.js
+++ reactOpenCourse/appeng/src/AppPart1.js
@@ -13,49 +13,47 @@ const Header = props => {
 const Content = props => {
   return (
     <div>
-      <p>
-        {props.content.name} {props.content.exercises}
-      </p>
+      {props.content.map(item => (
+        <p>
+          {item.name} {item.exercises}
+        </p>
+      ))}
     </div>
   );
 };
 
 const Total = props => {
+  function addUp(total, num) {
+    return total + num.exercises;
+  }
   return (
     <div>
-      <p>
-        Number of exercises{" "}
-        {props.exercises1 + props.exercises2 + props.exercises3}
-      </p>
+      <p>{props.exercises1.reduce(addUp, null)}</p>
     </div>
   );
 };
 function AppPart1() {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14
-  };
+  const part1 = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7
+    },
+    {
+      name: "State of a component",
+      exercises: 14
+    }
+  ];
 
   return (
     <div>
       <Header course={course} />
       <Content content={part1} />
-      <Content content={part2} />
-      <Content content={part3} />
-      <Total
-        exercises1={part1.exercises}
-        exercises2={part2.exercises}
-        exercises3={part3.exercises}
-      />
+      <Total exercises1={part1} />
     </div>
   );
 }
