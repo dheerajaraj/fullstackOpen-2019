7c5065836d2e208c0bf0eda0727dacfe0ac9e2fe
dheerajtaiger <dheeraj.r@taiger.com>
2020-01-30 09:42:58 +0800

    Finished Week1-Ex1.12

--- reactOpenCourse/appeng/src/AppPart1.js
+++ reactOpenCourse/appeng/src/AppPart1.js
@@ -1,79 +1,27 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import ReactDOM from "react-dom";
+const anecdotes = [
+  "If it hurts, do it more often",
+  "Adding manpower to a late software project makes it later!",
+  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  "Premature optimization is the root of all evil.",
+  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it."
+];
 
-function AppPart1() {
-  const [feedback, setFeedBack] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-    all: 0
-  });
-
-  const handleGoodFeedBack = () => {
-    setFeedBack({
-      ...feedback,
-      good: feedback.good + 1,
-      all: feedback.all + 1
-    });
+const AppPart1 = props => {
+  const [selected, setSelected] = useState(0);
+  const generateRandom = () => {
+    const newSelected = Math.round(Math.random() * anecdotes.length);
+    setSelected(newSelected);
   };
 
-  const handleNeutralFeedBack = () => {
-    setFeedBack({
-      ...feedback,
-      neutral: feedback.neutral + 1,
-      all: feedback.all + 1
-    });
-  };
-
-  const handleBadFeedBack = () => {
-    setFeedBack({
-      ...feedback,
-      bad: feedback.bad + 1,
-      all: feedback.all + 1
-    });
-  };
-
-  const Button = props => (
-    <button onClick={props.triggeredFeedBack}>{props.text}</button>
-  );
-
-  const Statistics = props => {
-    if (
-      (feedback.all === 0 && props.feedBackName === "average") ||
-      (feedback.all === 0 && props.feedBackName === "positive")
-    ) {
-      return <p>"No feedback given"</p>;
-    }
-    return (
-      <tr>
-        <td>{props.feedBackName}</td>
-        <td>{props.feedBackCount}</td>
-      </tr>
-    );
-  };
-
-  const average = (feedback.good - feedback.bad) / feedback.all;
-  const positivePercentage = (feedback.good / feedback.all) * 100 + " %";
   return (
     <div>
-      <h1> give feedback </h1>
-      <Button triggeredFeedBack={handleGoodFeedBack} text="good" />
-      <Button triggeredFeedBack={handleNeutralFeedBack} text="neutral" />
-      <Button triggeredFeedBack={handleBadFeedBack} text="bad" />
-      <h1> statistics </h1>
-      <table>
-        <Statistics feedBackName="good" feedBackCount={feedback.good} />
-        <Statistics feedBackName="neutral" feedBackCount={feedback.neutral} />
-        <Statistics feedBackName="bad" feedBackCount={feedback.bad} />
-        <Statistics feedBackName="all" feedBackCount={feedback.all} />
-        <Statistics feedBackName="average" feedBackCount={average} />
-        <Statistics
-          feedBackName="positive"
-          feedBackCount={positivePercentage}
-        />
-      </table>
+      <button onClick={generateRandom}>random</button>
+      {anecdotes[selected]}
     </div>
   );
-}
+};
+
 export default AppPart1;
