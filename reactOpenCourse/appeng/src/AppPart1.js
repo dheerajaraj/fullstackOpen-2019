ed42025ab0e129fcb6bbab5ff47ba4a8544492c9
dheerajtaiger <dheeraj.r@taiger.com>
2020-01-30 14:26:32 +0800

    Finished week1-Ex1.14

--- reactOpenCourse/appeng/src/AppPart1.js
+++ reactOpenCourse/appeng/src/AppPart1.js
@@ -10,27 +10,56 @@ const anecdotes = [
 ];
 
 const AppPart1 = props => {
-  const [selected, setSelected] = useState(0);
-  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
-  const updateVote = () => {
-    var newVote = [...votes];
-    newVote[selected] += 1;
-    setVotes(newVote);
-  };
+  const [selected, setSelected] = useState({
+    selection: 0,
+    votes: new Array(anecdotes.length).fill(0),
+    maxIndex: 0
+  });
+
   const generateRandom = () => {
     const newSelected = Math.round(Math.random() * (anecdotes.length - 1));
-    setSelected(newSelected);
+    setSelected({
+      ...selected,
+      selection: newSelected
+    });
   };
+
+  const updateVote = () => {
+    let newVote = [...selected.votes];
+    newVote[selected.selection] += 1;
+    let maximumIndex = popularAnecdote(newVote);
+    setSelected({
+      ...selected,
+      votes: newVote,
+      maxIndex: maximumIndex
+    });
+  };
+
   const Button = ({ onClick, text }) => (
     <button onClick={onClick}>{text}</button>
   );
 
+  let popularAnecdote = newVote => {
+    let maxIndex = 0;
+    let maxValue = newVote[0];
+    for (let i = 0; i < newVote.length; i++) {
+      if (newVote[i] > maxValue) {
+        maxValue = newVote[i];
+        maxIndex = i;
+      }
+    }
+    return maxIndex;
+  };
+
+  const ReportVotes = props => <p>has {props.totalVotes} votes</p>;
   return (
     <div>
-      {anecdotes[selected]}
-      <p>has {votes[selected]} votes</p>
-      <Button onClick={generateRandom} text="next anecdote" />
-      <Button onClick={updateVote} text="vote" />
+      {anecdotes[selected.selection]}
+      <ReportVotes totalVotes={selected.votes[selected.selection]} />
+      <Button onClick={() => generateRandom()} text="next anecdote" />
+      <Button onClick={() => updateVote()} text="vote" />
+      <p>{anecdotes[selected.maxIndex]}</p>
+      <ReportVotes totalVotes={selected.votes[selected.maxIndex]} />
     </div>
   );
 };
