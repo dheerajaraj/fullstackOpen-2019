b3c3244d018aedcf2643251e62ade263677cb33e
dheerajtaiger <dheeraj.r@taiger.com>
2020-02-01 19:29:45 +0800

    Finished week2-Ex2.2

--- reactOpenCourse/appeng/src/components/Parts.js
+++ reactOpenCourse/appeng/src/components/Parts.js
@@ -2,11 +2,23 @@ import React, { Component } from "react";
 
 class Parts extends Component {
   render() {
-    return this.props.course.parts.map(course => (
+    const result = this.props.course.parts.map(course => (
       <li key={course.id}>
         {course.name} {course.exercises}
       </li>
     ));
+    const totalResult = this.props.course.parts.reduce(function(
+      total,
+      currVal
+    ) {
+      return { exercises: total.exercises + currVal.exercises }; // need to be field of same name
+    });
+    return (
+      <div>
+        {result}
+        <p>total of {totalResult.exercises} exercises</p>
+      </div>
+    );
   }
 }
 export default Parts;
