2b357f300930cdc0735cbad96f2364002f239956
dheerajTaiger <dheeraj.r@taiger.com>
2020-03-02 01:32:14 +0800

    Deployment in progress

--- reactOpenCourse/appeng/src/components/CommunicationNotes.js
+++ reactOpenCourse/appeng/src/components/CommunicationNotes.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/notes";
+const baseUrl = "/api/notes";
 
 const insertPersonDetails = personEntry => {
   return axios.post(baseUrl, personEntry).then(response => {
